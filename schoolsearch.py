03766fda5de5ecec5677ba5f9777f469bc736310
Simon Ibssa <ibssasimon@Simons-iMac.attlocal.net>
2020-04-17 18:22:55 -0700

    finished structure of main

--- schoolsearch.py
+++ schoolsearch.py
@@ -15,10 +15,49 @@ class Student:
 def main():
     #Populate data from text file
     students = populateData("students.txt")
+
     # Write the prompts for user input
     writePrompts()
 
     # Ask for user input until user quits
+    while(1):
+        
+        args = input().split()
+        userInput = args[0]
+
+        if(userInput == "Q" or userInput == "Quit"):
+            break
+
+        elif userInput == "S" or userInput == "Student":
+            if(len(args) == 2):
+                # 1 additional argument passed in
+                searchStudent()
+            elif(len(args) == 3):
+                # 2 addtional arguments passed in
+                # Might have to parse for "B" in bus
+                searchStudent()
+
+        
+        elif userInput == "T" or userInput == "Teacher":
+            searchTeacher()
+        
+        elif userInput == "G" or userInput == "Grade":
+            searchGrade()
+
+        elif userInput == "B" or userInput == "Bus":
+            searchBus()
+
+        elif userInput == "A" or userInput == "Average":
+            searchAverage()
+
+        elif userInput == "I" or userInput == "Info":
+            searchInfo()
+        else:
+            print("Bad command")
+            continue
+
+        
+
     
 
 
@@ -46,6 +85,33 @@ def populateData(fileName):
         students.append(myStudent)
 
     return students
+
+
+
+def searchStudent():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
+
+
+def searchTeacher():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
+
+def searchGrade():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
+
+def searchBus():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
+
+def searchAverage():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
+
+def searchInfo():
+    pass
+    # If passed in bad parameters, print "Bad parameter", exit function
 
 
 
